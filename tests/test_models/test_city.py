53ed9a98f5e90812e4caf42469b93b1a30b9ade3
stuckwithprogression <chukwunwemichael@gmail.com>
2024-01-16 02:42:44 -0800

    test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import unittest
 
 
 class test_City(test_basemodel):
@@ -22,3 +23,37 @@ class test_City(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+
+class TestCity(unittest.TestCase):
+    """Tests for the City class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.city = City()
+
+    @classmethod
+    def teardown(cls):
+        del cls.city
+
+    def test_attributes_City(self):
+        """chekcing if City have attributes"""
+        self.assertTrue('id' in self.city.__dict__)
+        self.assertTrue('name' in self.city.__dict__)
+        self.assertTrue('state_id' in self.city.__dict__)
+        self.assertTrue('created_at' in self.city.__dict__)
+        self.assertTrue('updated_at' in self.city.__dict__)
+
+    def test_save_City(self):
+        """Test save method"""
+        self.city.save()
+        self.assertNotEqual(self.city.created_at, self.city.updated_at)
+
+    def test_to_dict_City(self):
+        """Test to_dict method"""
+        self.assertEqual('to_dict' in dir(self.city), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
